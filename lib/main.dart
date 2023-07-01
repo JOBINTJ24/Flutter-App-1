608da2d56b41656386bbf18259e348e8077d13ad
JOBINTJ24 <jobintj2023b@mca.ajce.in>
2023-07-01 18:47:27 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(
+  MaterialApp(
+    title: "My first app",
+    home: Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("Hello world"),
+        ),
+      ),
+    ),
+  ),
+);
+}
